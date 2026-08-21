7a1da9a6b80a215fb24960fb4abafec48f00dc98
Karel Planken <71339309+karelplanken@users.noreply.github.com>
2026-08-21 12:38:26 +0200

    chore: reviews and refactors challenge 11 - mile pace

--- challenges/cc_011_mile_pace.py
+++ challenges/cc_011_mile_pace.py
@@ -8,16 +8,59 @@
 # Add leading zeros when needed.
 from pytest import mark
 
+SECONDS_PER_MINUTE = 60
+
+
+def _parse_mmss(duration: str) -> int:
+    """Convert an "MM:SS" string to total seconds.
+
+    Args:
+        duration: Time in "MM:SS" format.
+
+    Returns:
+        Total number of seconds.
+    """
+    minutes, seconds = map(int, duration.split(':'))
+    return minutes * SECONDS_PER_MINUTE + seconds
+
+
+def _format_mmss(total_seconds: int) -> str:
+    """Convert total seconds to an "MM:SS" string with leading zeros.
+
+    Args:
+        total_seconds: Total number of seconds.
+
+    Returns:
+        Time in "MM:SS" format.
+    """
+    minutes, seconds = divmod(total_seconds, SECONDS_PER_MINUTE)
+    return f'{minutes:02}:{seconds:02}'
+
 
 def mile_pace(miles: float, duration: str) -> str:
-    minutes, seconds = map(int, duration.split(':'))
-    total_seconds = minutes * 60 + seconds
-    avg_seconds_per_mile = round(total_seconds / miles)
-    avg_minutes, avg_seconds = divmod(avg_seconds_per_mile, 60)
-    return f'{avg_minutes:02}:{avg_seconds:02}'
+    """Return average time per mile in "MM:SS" format.
+
+    Notice that banker's rounding is used to round the average time per mile.
+
+    Args:
+        miles: Number of miles run.
+        duration: Time taken to run the miles in "MM:SS" format.
+
+    Returns:
+        Average time per mile in "MM:SS" format.
+
+    Raises:
+        ValueError: If miles is not greater than zero.
+    """
+    if not miles > 0:
+        msg = 'miles must be greater than zero'
+        raise ValueError(msg)
+
+    avg_seconds_per_mile = round(_parse_mmss(duration) / miles)
+    return _format_mmss(avg_seconds_per_mile)
 
 
-tests = [
+tests: list[tuple[float, str, str]] = [
     (3, '24:00', '08:00'),
     (1, '06:45', '06:45'),
     (2, '07:00', '03:30'),
