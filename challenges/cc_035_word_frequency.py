3974d82b80cf109565013c68075a23d47cf03d83
Karel Planken <71339309+karelplanken@users.noreply.github.com>
2026-09-14 09:15:15 +0200

    chore: review and refactor challenge 35 - word frequency

--- challenges/cc_035_word_frequency.py
+++ challenges/cc_035_word_frequency.py
@@ -2,46 +2,45 @@
 
 # Word Frequency
 # Given a paragraph, return an array of the three most frequently occurring words.
-
-# Words in the paragraph will be separated by spaces.
-# Ignore case in the given paragraph. For example, treat Hello and hello as the same
-# word.
-# Ignore punctuation in the given paragraph. Punctuation consists of commas (,),
-# periods (.), and exclamation points (!).
-# The returned array should have all lowercase words.
-# The returned array should be in descending order with the most frequently occurring
-# word first.
+#
+# - Words in the paragraph will be separated by spaces.
+# - Ignore case in the given paragraph. For example, treat Hello and hello as the same
+#   word.
+# - Ignore punctuation in the given paragraph. Punctuation consists of commas (,),
+#   periods (.), and exclamation points (!).
+# - The returned array should have all lowercase words.
+# - The returned array should be in descending order with the most frequently occurring
+#   word first.
 from collections import Counter
 
 from pytest import mark
 
+MAX_WORDS = 3
+PUNCTUATION_TO_STRIP = ',.!'
+REMOVE_PUNCTUATION = str.maketrans('', '', PUNCTUATION_TO_STRIP)
+
 
 def get_words(paragraph: str) -> list[str]:
-    clean_text = paragraph.translate(str.maketrans('', '', ',.!'))
-    words = [word.lower() for word in clean_text.split() if word]
+    """Return the three most frequently occurring words in the given paragraph.
 
-    return [word for word, _ in Counter(words).most_common(3)]
+    Args:
+        paragraph: Text containing words of any case and punctuation.
 
+    Returns:
+        A list of maximum three most frequently occurring words in descending order
+        and in lowercase. Ties are broken by order of first appearance in the
+        paragraph.
 
-# # Alternatives for the maketrans and translate approach:
-# # Option 1: List comprehension (very explicit)
-# clean_text = ''.join(char for char in paragraph if char not in ',.!')
+    Raises:
+        ValueError: If the paragraph is empty.
+    """
+    if not paragraph:
+        msg = 'paragraph must not be empty'
+        raise ValueError(msg)
 
-# # Option 2: Regular expressions (powerful but overkill for this)
-# import re
+    words = paragraph.translate(REMOVE_PUNCTUATION).lower().split()
+    return [word for word, _ in Counter(words).most_common(MAX_WORDS)]
 
-# clean_text = re.sub(r'[,.!]', '', paragraph)
-
-# # Option 3: Chained replace (readable but slower)
-# clean_text = paragraph.replace(',', '').replace('.', '').replace('!', '')
-
-
-# # Option 4: Using a simple helper function
-# def remove_chars(text, chars):
-#     return text.translate(str.maketrans('', '', chars))
-
-
-# clean_text = remove_chars(paragraph, ',.!')  # More readable!
 
 tests = [
     (
@@ -65,4 +64,4 @@ def test_get_words(paragraph: str, expected: list[str]) -> None:
 
 if __name__ == '__main__':
     paragraph, expected = tests[0]
-    get_words(paragraph)
+    print(get_words(paragraph))
